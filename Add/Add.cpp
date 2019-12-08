1e4b0e5ceb057cbafbd0ba8e2af0a0fa11ac0a33
Maciej Drozdowicz <Maciej.Drozdowicz@wit.edu.pl>
2019-12-08 14:32:13 +0100

    Błąd z wyswietlaniem 0,5

--- Add/Add.cpp
+++ Add/Add.cpp
@@ -11,11 +11,10 @@ void print(std::vector<double> const &input)
 	for (int i = 0; i < input.size(); i++) {
 		std::cout << input.at(i) << ' ';
 	}
+	std::cout << endl;
 }
 
 vector<double> operator+(const vector<double>& lhs, const vector<double>& rhs) {	// return type is a vector of integers
-
-
 	vector<double> result;	// Declaring the resulting vector, result
 	int resultSize = lhs.size() + rhs.size();
 
@@ -25,16 +24,22 @@ vector<double> operator+(const vector<double>& lhs, const vector<double>& rhs) {
 	for (double item : rhs) {
 		result.push_back(item);
 	}
-
 	return result;	// returning the vector "result"
-
 }
 
+vector<double>& operator+=(vector<double>& lhs, const vector<double>& rhs) {	
+	for (int item : rhs) {
+		lhs.push_back(item);
+	}
+	return lhs;
+}
 
 int main()
 {
-	cout << "Hello world!" << endl;
-	std::vector<double> test = std::vector<double>{ 6.7, -12.3, 5 } +std::vector<double> {0.5, -2.7, 19};
+	print(std::vector<double> {6.7, -12.3, 5} +std::vector<double> {0.5, -2.7, 19});
+	std::vector<double> vector{ 6.7, -12.3, 5 };
+	print((vector += std::vector<double> {0.5, -2.7}) += std::vector<double> {19});
+	print(vector);
 	return 0;
 }
 
